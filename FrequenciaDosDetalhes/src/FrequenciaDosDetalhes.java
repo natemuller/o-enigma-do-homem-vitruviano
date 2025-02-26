34595df8f43f818b70b040cfb717338b21ff5484
Nate <natan.muller@edu.pucrs.br>
2025-02-26 17:52:41 -0300

    implementação provisória do método contaOcorrenciaIndividual, ainda sem corrigir bugs

--- FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
+++ FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
@@ -23,6 +23,20 @@ public class FrequenciaDosDetalhes {
         return false;
     }
 
+    public void contaOcorrenciaIndividual (int [] arrayElementos) {
+        int contagem = 0;
+        for (int i = 0; i < arrayElementos.length; i++) {
+            for (int j = i + 1; j < arrayElementos.length; j++) {
+                if (arrayElementos[i] == arrayElementos[j]) {
+                     System.out.println("O numero" + i + "se repete no array");
+                    contagem++;
+                     continue;
+                }
+            }
+        }
+        System.out.println("O array possui " + contagem + " elementos que se repetem");
+    }
+
     public static void main (String[] args) throws Exception {
         System.out.println ("De os elementos do array separados por espaco: ");
         Scanner scanner = new Scanner (System.in);
@@ -41,5 +55,7 @@ public class FrequenciaDosDetalhes {
         System.out.println("O array possui ocorrencias? " + frequencia.verificaOcorrencia(array));
 
         frequencia.contaOcorrencia(array, num);
+
+        frequencia.contaOcorrenciaIndividual(array);
     }
 }
