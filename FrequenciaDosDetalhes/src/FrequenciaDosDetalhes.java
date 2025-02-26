73aed5b645d30cf5c438c5deb9b536c61e1c19ce
Nate <natan.muller@edu.pucrs.br>
2025-02-26 14:56:38 -0300

    topico M02.1 corretamente implementado

--- FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
+++ FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class FrequenciaDosDetalhes {
 
     public void verificaOcorrencia (int [] arrayElementos, int num) {
-
+        int frequencia = 0;
+        for (int i = 0; i < arrayElementos.length; i++) {
+            if (arrayElementos[i] == num) {  
+                frequencia++;
+            }
+        }
+        System.out.println ("O array acima possui " + frequencia + " ocorrencias do numero " + num);
     }
     public static void main (String[] args) throws Exception {
         System.out.println ("De os elementos do array separados por espaco: ");
@@ -18,7 +24,9 @@ public class FrequenciaDosDetalhes {
 
         System.out.println(Arrays.toString(array));
         System.out.println(" ");
-        System.out.println("O array acima possui " + " ocorrencias do numero " + num);
+
+        FrequenciaDosDetalhes frequencia = new FrequenciaDosDetalhes ();
+        frequencia.verificaOcorrencia (array, num);
         
         
     }
