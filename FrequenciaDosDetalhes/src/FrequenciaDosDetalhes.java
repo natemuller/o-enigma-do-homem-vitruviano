9c68bdcef976e538595ed5d8719892866feca96a
Nate <natan.muller@edu.pucrs.br>
2025-02-26 14:40:35 -0300

    falta impoementar o metodo verificaOcorrencia (linha 5), já montei a main solicitando os elementos do array  para o usuario, imprimindo ele e, solicitando o indice para verificar o numero de ocorrencias

--- FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
+++ FrequenciaDosDetalhes/src/FrequenciaDosDetalhes.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class FrequenciaDosDetalhes {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public void verificaOcorrencia (int [] arrayElementos, int num) {
+
+    }
+    public static void main (String[] args) throws Exception {
+        System.out.println ("De os elementos do array separados por espaco: ");
+        Scanner scanner = new Scanner (System.in);
+        String elementos = scanner.nextLine ();
+
+        int[] array = Arrays.stream(elementos.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println ("De o numero que desejas saber a frequencia: ");
+        int num = scanner.nextInt ();
+        scanner.close ();
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(" ");
+        System.out.println("O array acima possui " + " ocorrencias do numero " + num);
+        
+        
     }
 }
