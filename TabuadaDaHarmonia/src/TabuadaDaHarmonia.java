09d2072676a0c970123d79e913216b8e732cc851
Nate <natan.muller@edu.pucrs.br>
2025-03-05 17:14:23 -0300

    implementação dos metodos CalculaTabuada e CalculaPotencia, tudo corretamente implementado, restando somente as atividades m03.3 e m03.4

--- TabuadaDaHarmonia/src/TabuadaDaHarmonia.java
+++ TabuadaDaHarmonia/src/TabuadaDaHarmonia.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class TabuadaDaHarmonia {
+    public static void CalculaTabuada (int valor) {
+        System.out.printf("Tabuada de %d:", valor);
+        System.out.println(" ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", valor, i, valor * i);
+        }
+    }
+
+    public static void CalculaPotencia (int a, int b) {
+        int resultado = (int) Math.pow(a, b);
+        System.out.printf("%d ^ %d = %d", a, b, resultado);
+        System.out.println (" ");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int valor = 0;
+        int a = 0;
+        int b = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("De um inteiro:");
+        System.out.println(" ");
+        valor = scanner.nextInt();        
+
+        CalculaTabuada(valor);
+
+        System.out.printf("De dois inteiros separados por enter:");
+        System.out.println(" ");
+        a = scanner.nextInt();     
+        b = scanner.nextInt();     
+        scanner.close(); 
+
+        CalculaPotencia(a, b);
     }
 }
